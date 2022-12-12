e2b56cc3aba3ec4fab5ac43a816a7e0b10c5abea
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2022-12-11 23:17:50 -0500

    revised ussd app

--- ussd.php
+++ ussd.php
@@ -7,10 +7,10 @@
     * When working from a live server change the GET[] method 
     * to POST[] (that is how africastalking do their stuff) 
     */ 
-    $phone = $_POST['phoneNumber'];
-    $session_id = $_POST['sessionId'];
-    $service_code = $_POST['serviceCode'];
-    $ussd_string= $_POST['text'];
+    $phone = $_GET['phoneNumber'];
+    $session_id = $_GET['sessionId'];
+    $service_code = $_GET['serviceCode'];
+    $ussd_string = $_GET['text'];
 
 
     /*
@@ -65,79 +65,118 @@
     }
 
     function crop_production($ussd_string_exploded) {
-        if (count($ussd_string_exploded) == 1) {
-            $ussd_string = "CON Please Select: \n \n 1. Maize production \n 2. Groundnuts production \n";
+
+
+
+        if (count($ussd_string_exploded) == 1 ) {
+            $ussd_string = "Please Select: \n \n 1. Maize production \n 2. Groundnuts production \n";
+            ussd_proceed($ussd_string);	  
+        }
+
+        if (count($ussd_string_exploded) == 2) {
+            $option = $ussd_string_exploded[1];
+
+            if($option == "1") {
+                $ussd_string = "Please Select: \n \n1. SC 719(Njobvu) \n2. SC 627 (Mkango)\n";
+                ussd_proceed($ussd_string); 
+
+                
+
+                
+
+            }
+            else if ($option == "2") {
+                $ussd_string = "Select your groundnuts variety: \n \n1. Chalimbana\n";
+                ussd_proceed($ussd_string);
+            }
+
+            else {
+                echo "Invalid input";
+            }
+            
+
+            
+                
+            
+        }
+
+        if (count($ussd_string_exploded) == 3) {
+            $ussd_string = "Please Select: \n \n1. Husbandry practices \n2. Pests and dieseases \n";
             ussd_proceed($ussd_string);
 
-            if (count($ussd_string_exploded) == 2) {
-                switch ($ussd_string_exploded[1]) {
-                    case 1:
-                        $ussd_string = "Please Select: \n \n1. SC 719(Njobvu) \n2. SC 627 (Mkango)\n";
-                        ussd_proceed($ussd_string);
 
-                        if (count($ussd_string_exploded) == 3) {
-                            $ussd_string = "Please Select: \n \n1. Husbandry practices \n2. Pests and dieseases \n";
-                            ussd_proceed($ussd_string);
+        }
 
-                            if (count($ussd_string_exploded) == 4) {
-                                switch ($ussd_string_exploded[3]) {
-                                    case 1:
-                                        $ussd_string = "Clear the land as early as possible before November for summer crop.\n";
-                                        $ussd_string .= "Make ridges or rows at 60 - 75cm.\nEnter N for next";
-                                        ussd_proceed($ussd_string);
+        if(count($ussd_string_exploded) == 4) {
+            $option = $ussd_string_exploded[3];
 
-                                        if (count($ussd_string_exploded) == 5) {
-                                            $ussd_string = "Plant one seed per station, spaced at 25 - 30cm.\n";
-                                            $ussd_string .= "Seed need around 25kg/ha or 10kg/acre.\nEnter N for next";
-                                            ussd_proceed($ussd_string);
+            if($option == "1") {
+                $ussd_string = "Clear the land as early as possible before November for summer crop.\n";
+                $ussd_string .= "Make ridges or rows at 60 - 75cm.\nEnter N for next";
+                ussd_proceed($ussd_string);
 
-                                            if (count($ussd_string_exploded) == 6) {
-                                                $ussd_string = "Apply 100 kg basal fertilizer preferably 23:21:0 + 4s at 5gms / plant just after emegency.\n";
-                                                $ussd_string .= "Keep the field weed free all the time.\nEnter N for next";
-                                                ussd_proceed($ussd_string);
-
-                                                if (count($ussd_string_exploded) == 7) {
-                                                    $ussd_string = "Apply top dressing fertlizer like urea, when plants are 30cm high or 21 days after emergency.\n";
-                                                    $ussd_string .= "Keep the field weed free all the time.\nEnter N for next";
-                                                    ussd_proceed($ussd_string);
-                                                    
-                                                    if (count($ussd_string_exploded) == 8) {
-                                                        $ussd_string = "Harvest when the crop is at 18% moisture content and leave it to dry in granaries.\n";
-                                                        $ussd_string .= "Enter N for next";
-                                                        ussd_proceed($ussd_string);
-
-                                                        if (count($ussd_string_exploded) == 9) {
-                                                            $ussd_string = "Shell and bag when the crop is at 12.5% moisture content. Remember to treat with pesticide to avoid weavil.\n";
-                                                            $ussd_string .= "Thank you for using Ulimi wathu app";
-                                                            ussd_stop($ussd_string);  
-
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        break;
-
-                                    case 2:
-                                        break;    
-                                }  
-                            } 
-                        }     
-                        break;    
-
-                    case 2:
-                        $ussd_string = "Select your groundnuts variety: \n \n1. Chalimbana\n";
-                        ussd_proceed($ussd_string);
-                        break; 
-                    
-                    default:
-                            echo "Invalid response";
-                            break;
-
-
-                }
+                
             }
-        }   
+
+            else if ($option == "2") {
+                echo "Paste and diseases menu";
+            }
+
+            else {
+                echo "Invalid input";
+            }
+        }
+        if (count($ussd_string_exploded) == 5) {
+            $option = $ussd_string_exploded[4];
+
+            if($option == "N" || $option == "n") {
+                $ussd_string = "Plant one seed per station, spaced at 25 - 30cm.\n";
+                $ussd_string .= "Seed need around 25kg/ha or 10kg/acre.\nEnter N for next";
+                ussd_proceed($ussd_string);
+            }
+
+        }
+        if (count($ussd_string_exploded) == 6) {
+            $option = $ussd_string_exploded[5];
+
+            if($option == "N" || $option == "n") {
+                $ussd_string = "Apply 100 kg basal fertilizer preferably 23:21:0 + 4s at 5gms / plant just after emegency.\n";
+                $ussd_string .= "Keep the field weed free all the time.\nEnter N for next";
+                ussd_proceed($ussd_string);
+            }
+        }
+        else if (count($ussd_string_exploded) == 7) {
+            $option = $ussd_string_exploded[6];
+
+            if($option == "N" || $option == "n") {
+                $ussd_string = "Apply top dressing fertlizer like urea, when plants are 30cm high or 21 days after emergency.\n";
+                $ussd_string .= "Enter N for next";
+                ussd_proceed($ussd_string);
+            }
+
+        }
+        else if (count($ussd_string_exploded) == 8) {
+            $option = $ussd_string_exploded[7];
+
+            if($option == "N" || $option == "n") {
+                $ussd_string = "Harvest when the crop is at 18% moisture content and leave it to dry in granaries.\n";
+                $ussd_string .= "Enter N for next";
+                ussd_proceed($ussd_string);
+            }
+
+        }
+        else if (count($ussd_string_exploded) == 9) {
+            $option = $ussd_string_exploded[8];
+
+            if($option == "N" || $option == "n") {
+                $ussd_string = "Shell and bag when the crop is at 12.5% moisture content. Remember to treat with pesticide to avoid weavil.\n";
+                $ussd_string .= "Thank you for using Ulimi wathu app";
+                ussd_stop($ussd_string);
+            }
+
+        }
+
+       
     }
 
     function animal_production($ussd_string_exploded) {
