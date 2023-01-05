7773af51863d410d6dd34251a5cc494ef04f9f90
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-05 07:39:19 +0200

    send sms to one number

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import africastalking from "africastalking";
+
+const mysql = require("mysql");
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "programmer",
+  password: "Cyberman@2999",
+  database: "users",
+  port: "3302",
+});
+
+conn.connect(function (err) {
+  if (err) throw err;
+  conn.query("SELECT phone_number FROM user", function (err, results, fields) {
+    if (err) throw err;
+
+    const client = africastalking({
+      apiKey:
+        "0667ea64781b9fc514a0f44ca00611cd136be59c7783ec2d7d05ac6cf1ba4df0", // use your sandbox app API key for development in the test environment
+      username: "sandbox",
+    });
+
+    client.SMS.send({
+      to: "+26588234314",
+      message: "Hey there, we are testing ulimi wathu app",
+      from: "ULIMI-WATHU",
+    })
+      .then(() => console.log("Message sent successifully"))
+      .catch((err) => console.log(err));
+  });
+});
